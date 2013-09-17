a517fa02e361137d620203fcc276f3ea53bcb826
Season <blueseason@gmail.com>
2013-09-17 23:30:43 +0800

    add some test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -90,7 +90,7 @@ describe "User pages" do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Confirm Password", with: "foobar"
       end
 
       it "should create a user" do
@@ -114,7 +114,7 @@ describe "User pages" do
       sign_in user
       visit edit_user_path(user)
     end
-    
+
     describe "forbidden attributes" do
       let(:params) do
         { user: { admin: true, password: user.password,
